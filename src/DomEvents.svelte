d8032dadc0f5d477602029a40f5d0c9fea6406e4
adityavshahi <avshahi2001@gmail.com>
2024-04-24 01:54:19 +0530

    more dom events

--- src/DomEvents.svelte
+++ src/DomEvents.svelte
@@ -6,8 +6,14 @@
     function keyPressed(e) {
         letter = e.key;
     }
+    function handleSubmit(e) {
+        console.log('Form submitted!!');
+    }
 </script>
 
 <button on:click={clickMe}>Click Me</button>
 <svelte:window on:keypress={keyPressed} />
 <h3>{letter}</h3>
+<form on:submit|preventDefault={handleSubmit}>
+    <button type="submit">Submit</button>
+</form>
