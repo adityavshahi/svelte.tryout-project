26c189d0efe94de2975dd6b8099a9abe5b12b013
adityavshahi <avshahi2001@gmail.com>
2024-05-07 13:56:06 +0530

    custom events 1

---
+++ src/Form.svelte
@@ -0,0 +1,17 @@
+<script>
+    let name = '';
+    let age = null;
+    import {createEventDispatcher} from 'svelte';
+
+    function onSubmit(e) {
+        console.log("Form Submitted!!");
+    }
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+    <label for="name">Name: </label>
+    <input type="text" bind:value={name} id="name">
+    <label for="age" >Age: </label>
+    <input type="number" bind:value={age} id="age">
+    <button type="submit">Submit</button>
+</form>
